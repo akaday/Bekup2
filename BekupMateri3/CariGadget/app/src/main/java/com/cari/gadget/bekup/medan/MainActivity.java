e206926e842635b1ef28960cbc3aab6aac1cbc1f
akaday <cakra.ds@gmail.com>
2016-11-02 09:54:09 +0700

    all done, - detail

--- BekupMateri3/CariGadget/app/src/main/java/com/cari/gadget/bekup/medan/MainActivity.java
+++ BekupMateri3/CariGadget/app/src/main/java/com/cari/gadget/bekup/medan/MainActivity.java
@@ -9,11 +9,9 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
-
-import com.cari.gadget.bekup.medan.adapter.PhoneAdapter;
-import com.cari.gadget.bekup.medan.setget.Phone;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
@@ -28,118 +26,56 @@ import org.json.JSONObject;
 import java.io.IOException;
 import java.util.ArrayList;
 
+import static android.R.attr.button;
 
 public class MainActivity extends AppCompatActivity {
-
-    ArrayList<Phone> phoneList;
-
-    PhoneAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        phoneList = new ArrayList<Phone>();
-        new JSONAsyncTask().execute("http://ibacor.com/api/gsm-arena?view=brand&slug=samsung&page=1");
-
-        ListView listview = (ListView) findViewById(R.id.lv_item);
-        adapter = new PhoneAdapter(MainActivity.this, R.layout.lsv_item_phone, phoneList);
-
-        listview.setAdapter(adapter);
-
-        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-
-            @Override
-            public void onItemClick(AdapterView<?> arg0, View arg1, int position,
-                                    long id) {
-                // TODO Auto-generated method stub
-                String kodeslug =  String.valueOf(phoneList.get(position).getSlug());
-//                Log.d("KODENYA" , kodeslug);
-                Intent pindah = new Intent(MainActivity.this, DetailPhone.class);
-                pindah.putExtra("slug", kodeslug);
-                startActivity(pindah);
-
-
-            }
-        });
-
-
-
-
-
-
     }
 
-    class JSONAsyncTask extends AsyncTask<String, Void, Boolean> {
-
-
-        ProgressDialog dialog;
-
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            dialog = new ProgressDialog(MainActivity.this);
-            dialog.setMessage("Sedang Mengambil Data...");
-            dialog.show();
-            dialog.setCancelable(false);
-        }
-
-        @Override
-        protected Boolean doInBackground(String... urls) {
-            try {
-
-                //------------------>>
-                HttpGet httppost = new HttpGet(urls[0]);
-                HttpClient httpclient = new DefaultHttpClient();
-                HttpResponse response = httpclient.execute(httppost);
-
-                // StatusLine stat = response.getStatusLine();
-                int status = response.getStatusLine().getStatusCode();
-
-                if (status == 200) {
-                    HttpEntity entity = response.getEntity();
-                    String data = EntityUtils.toString(entity);
-
-
-                    JSONObject jsono = new JSONObject(data);
-                    JSONArray jarray = jsono.getJSONArray("data");
-
-                    for (int i = 0; i < jarray.length(); i++) {
-                        JSONObject object = jarray.getJSONObject(i);
-
-                        Phone phone = new Phone();
-                        phone.setTitle(object.getString("title"));
-                        phone.setImage(object.getString("img"));
-                        phone.setSlug(object.getString("slug"));
-                        phoneList.add(phone);
-                    }
-                    return true;
-                }
-
-                //------------------>>
-
-            } catch (ParseException e1) {
-                e1.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-            return false;
-        }
-
-        protected void onPostExecute(Boolean result) {
-            dialog.cancel();
-            adapter.notifyDataSetChanged();
-            if(result == false)
-                Toast.makeText(MainActivity.this, "Unable to fetch data from server", Toast.LENGTH_LONG).show();
-
-        }
+    public void goToPage(View v) {
+        // 1) Possibly check for instance of first
+        Button b = (Button)v;
+        String namabrand = b.getText().toString();
+        Log.d("KODENYA : " , namabrand);
+        Intent pindah = new Intent(MainActivity.this, ListPhone.class);
+        pindah.putExtra("brand", namabrand);
+        startActivity(pindah);
     }
 
+    /*public void goToSamsung(View view) {
+        // TODO Auto-generated method stub
+        String namabrand =  "samsung";
+        Intent pindah = new Intent(MainActivity.this, ListPhone.class);
+        pindah.putExtra("brand", namabrand);
+        startActivity(pindah);
+    }
 
+    public void goToAsus(View view) {
+        // TODO Auto-generated method stub
+        String  namabrand =  "asus";
+        Intent  pindah   = new Intent(MainActivity.this, ListPhone.class);
+                pindah.putExtra("brand", namabrand);
+        startActivity(pindah);
+    }
 
+    public void goToLenovo(View view) {
+        // TODO Auto-generated method stub
+        String namabrand =  "lenovo";
+        Intent pindah = new Intent(MainActivity.this, ListPhone.class);
+        pindah.putExtra("brand", namabrand);
+        startActivity(pindah);
+    }
 
+    public void goToApple(View view) {
+        // TODO Auto-generated method stub
+        String namabrand =  "apple";
+        Intent pindah = new Intent(MainActivity.this, ListPhone.class);
+        pindah.putExtra("brand", namabrand);
+        startActivity(pindah);
+    }*/
 
 }
